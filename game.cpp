3179ed2c6491fa61973e65ece78b9f6bce8704b3
domsel19 <dominiksalge@outlook.de>
2024-01-29 17:40:49 +0100

    Fixed setcross and setcirc function

--- game.cpp
+++ game.cpp
@@ -5,8 +5,10 @@
 class Field {
  private:
   std::vector<std::vector<std::string>> field;
+  int player = 0;
 
  public:
+  int winner = -1;
   Field() : field(5, std::vector<std::string>(5, " ")) {
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < 5; j++) {
@@ -22,19 +24,23 @@ class Field {
     }
   }
   void setcross(int r, int c) {
-    if (r % 2 == 0 && c % 2 == 0) {
+    if (r % 2 == 0 && c % 2 == 0 && field[r][c] == " ") {
       field[r][c] = "X";
+      player++;
+      wino();
     }
   }
   void setcirc(int r, int c) {
-    if (r % 2 == 0 && c % 2 == 0) {
+    if (r % 2 == 0 && c % 2 == 0 && field[r][c] == " ") {
       field[r][c] = "O";
+      player++;
+      winx();
     }
   }
   void reset() {
     for (int i = 0; i < 5; i += 2) {
       for (int j = 0; j < 5; j += 2) {
-        field[i][j] = "";
+        field[i][j] = " ";
       }
     }
   }
@@ -47,13 +53,54 @@ class Field {
     }
     std::cout << std::endl;
   }
+  void winx() {
+    if (field[0][0] == "X" && field[0][2] == "X" && field[0][4] == "X") {
+      winner = 1;
+    } else if (field[0][0] == "X" && field[2][2] == "X" && field[4][4] == "X") {
+      winner = 1;
+    } else if (field[2][0] == "X" && field[2][2] == "X" && field[2][4] == "X") {
+      winner = 1;
+    } else if (field[4][0] == "X" && field[4][2] == "X" && field[4][4] == "X") {
+      winner = 1;
+    } else if (field[2][0] == "X" && field[2][2] == "X" && field[2][4] == "X") {
+      winner = 1;
+    } else if (field[0][0] == "X" && field[2][0] == "X" && field[4][0] == "X") {
+      winner = 1;
+    }
+  }
+  void wino() {
+    if (field[0][0] == "O" && field[0][2] == "O" && field[0][4] == "O") {
+      winner = 2;
+    } else if (field[0][0] == "O" && field[2][2] == "O" && field[4][4] == "O") {
+      winner = 2;
+    } else if (field[2][0] == "O" && field[2][2] == "O" && field[2][4] == "O") {
+      winner = 2;
+    } else if (field[4][0] == "O" && field[4][2] == "O" && field[4][4] == "O") {
+      winner = 2;
+    } else if (field[2][0] == "O" && field[2][2] == "O" && field[2][4] == "O") {
+      winner = 2;
+    } else if (field[0][0] == "O" && field[2][0] == "O" && field[4][0] == "O") {
+      winner = 2;
+    }
+  }
+  void turn(int a, int b) {
+    if (player % 2 == 0) {
+      setcross(a, b);
+    } else {
+      setcirc(a, b);
+    }
+  }
 };
 int main() {
   Field gamefield;
-  gamefield.print();
-  gamefield.setcross(0, 0);
-  gamefield.print();
-  gamefield.reset();
-  gamefield.print();
+  int a, b;
+  while (gamefield.winner == -1) {
+    gamefield.print();
+    std::cout << "Enter the values of your next choice of field with coma "
+                 "seperation: ";
+    std::cin >> a >> b;
+    gamefield.turn(a, b);
+  }
+  printf("The winner is Player: %i \n", gamefield.winner);
   return 0;
 }
