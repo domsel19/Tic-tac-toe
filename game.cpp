70770828ff3c19ed6d6bca35e42417d23a170a9b
domsel19 <dominiksalge@outlook.de>
2024-02-02 17:32:35 +0100

    corrected mistakes and programm finished.

--- game.cpp
+++ game.cpp
@@ -9,7 +9,7 @@ class Field {
   std::vector<std::vector<Symbol>> field;
 
  public:
-  Symbol player;
+  Symbol player = Symbol::X;
   Field() : field(3, std::vector<Symbol>(3, Symbol::Empty)) {}
 
   void printBoard() {
@@ -63,20 +63,37 @@ class Field {
       field[row][col] = player;
     }
   }
+  bool check_field(int a, int b) { return field[a][b] == Symbol::Empty; }
 };
 int main() {
   int a, b;
   Field gamefield;
-  while (gamefield.game_over() == false) {
+  while (!gamefield.game_over()) {
     char gamewinner;
     gamefield.printBoard();
-    std::cout << "Enter the row and collum you want to take your turn on: ";
+    std::cout << "Enter the row and column you want to take your turn on: ";
     std::cin >> a >> b;
+    if (std::cin.fail() || a < 0 || a >= 3 || b < 0 || b >= 3 ||
+        gamefield.check_field(a, b) != true) {
+      std::cout
+          << "Your input was invalid. Please enter the row and column again."
+          << std::endl;
+      std::cin.clear();
+      std::cin >> a >> b;
+      continue;
+    }
+
     gamefield.turn(a, b, gamefield.player);
     if (gamefield.winner(gamefield.player) == true) {
-      if (gamefield.player == Symbol::X ? gamewinner = 'X' : gamewinner = 'O')
-        std::cout << "The winner is: " << gamewinner << std::endl;
+      gamewinner = (gamefield.player == Symbol::X) ? 'X' : 'O';
+      gamefield.printBoard();
+      std::cout << "The winner is: " << gamewinner << std::endl;
       break;
+    }
+    if (gamefield.player == Symbol::X) {
+      gamefield.player = Symbol::O;
+    } else {
+      gamefield.player = Symbol::X;
     }
   }
 
